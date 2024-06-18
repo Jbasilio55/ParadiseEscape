cc56093bdc6f8a3b02112970a048bf55a4418ba0
Jbasilio55 <j.l.basilio55@gmail.com>
2024-06-18 16:24:39 -0400

    starter files added

---
+++ paradiseEscapeBackend/src/main/java/tech/jorgebasilio/paradiseescapebackend/ParadiseEscapeBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.jorgebasilio.paradiseescapebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadiseEscapeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParadiseEscapeBackendApplication.class, args);
+    }
+
+}
---
+++ paradiseEscapeBackend/src/test/java/tech/jorgebasilio/paradiseescapebackend/ParadiseEscapeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jorgebasilio.paradiseescapebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadiseEscapeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
